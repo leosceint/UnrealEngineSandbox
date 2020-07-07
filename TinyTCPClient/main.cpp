396fce9de33e50bab0a177b310c8d8bd26f6f629
Lev Grodzickiy <piyaotoi@gmail.com>
2020-07-07 09:54:55 +0300

    Модификация TinyTCPClient в целях тестирования

--- TinyTCPClient/main.cpp
+++ TinyTCPClient/main.cpp
@@ -1,5 +1,6 @@
 #include "CanData.h"
 #include <iostream>
+#include <iomanip>
 #include <WinSock2.h>
 #include <thread>
 
@@ -35,15 +36,24 @@ int main()
         
         unsigned int IdxSent = 0;
         unsigned int type_appendix = 0;
+
         while(Data.time < 20000)
         {
             Data.type = 0x0101 + type_appendix;
             result = send(connection, (char*)&Data, sizeof(CanData), 0);
             if (result == SOCKET_ERROR)
                 break;
-            cout << "!! Data sent #";
-            this_thread::sleep_for(4ms);
-            cout << IdxSent << endl;
+            cout << "!! 1. Data sent #";
+            result = send(connection, (char*)&Data, sizeof(CanData), 0);
+            if (result == SOCKET_ERROR)
+                break;
+            cout << dec << IdxSent;
+            this_thread::sleep_for(2ms);
+            cout << " !! 2. Data sent #";
+            this_thread::sleep_for(2ms);
+            cout << dec << IdxSent;
+            cout << " <type> -- " << hex << setfill('0')
+            << setw(4) << showbase << Data.type << endl;
             ++Data.time;
             ++IdxSent;
             ++type_appendix;
